f81c4cff909b4851f07aea4e4f7500d4264e806a
jon <jonathan-langlois@live.ca>
2020-07-22 16:44:16 -0700

    setup file upload

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask,flash,request,redirect,send_file,render_template
+app = Flask(__name__, template_folder='templates')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+      if 'file' not in request.files:
+        return 'No file sent.'
+      return "Got File."
 
-@app.route('/')
-def init():
-    return 'init'
+@app.route('/', methods=['GET'])
+def index():
+  return render_template('landing.html')
