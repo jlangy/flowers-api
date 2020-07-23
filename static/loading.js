a2cb5eaa3178cf782dc474733207c3e0be4758d0
Jon Langlois <jonathan-langlois@live.ca>
2020-07-22 21:45:28 -0700

    add loading spinner

---
+++ static/loading.js
@@ -0,0 +1,20 @@
+function createSpinner(){
+  const accessibilitySpan = document.createElement("SPAN");
+  accessibilitySpan.classList.add("sr-only");
+  const spinner = document.createElement("DIV");
+  spinner.classList.add("spinner-border");
+  spinner.setAttribute("role", "status");
+  spinner.appendChild(accessibilitySpan);
+  return spinner;
+}
+
+
+window.onload = function(){
+  const uploadFileBtn = document.getElementById("upload-file-button");
+  uploadFileBtn.onclick = function(){
+    uploadFileBtn.innerHTML = "";
+    uploadFileBtn.appendChild(createSpinner());
+    uploadFileBtn.setAttribute("disabled", true)
+  }
+}
+
